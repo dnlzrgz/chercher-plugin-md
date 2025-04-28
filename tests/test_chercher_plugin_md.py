ef16b1bd3ef03f60b1ae62670f3a4f407f6ce37b
dnlzrgz <24715931+dnlzrgz@users.noreply.github.com>
2025-04-28 11:48:06 +0200

    feat: handle file uri and relative uri

--- tests/test_chercher_plugin_md.py
+++ tests/test_chercher_plugin_md.py
@@ -11,7 +11,7 @@ And how to do it in production.
 """
 
 
-def test_valid_file(tmp_path):
+def test_valid_file_with_file_uri(tmp_path):
     p = tmp_path / "test.md"
     p.write_text(CONTENT)
 
@@ -21,6 +21,23 @@ def test_valid_file(tmp_path):
     for doc in documents:
         assert isinstance(doc, Document)
         assert doc.uri == uri
+        assert doc.title == p.stem
+        assert doc.body != ""
+        assert isinstance(doc.metadata, dict)
+        assert doc.metadata["title"] == "TDD"
+        assert doc.hash is not None
+
+
+def test_valid_file_with_relative_uri(tmp_path):
+    p = tmp_path / "test.md"
+    p.write_text(CONTENT)
+
+    uri = p.as_posix()
+    documents = list(ingest(uri=uri))
+    assert len(documents) == 1
+    for doc in documents:
+        assert isinstance(doc, Document)
+        assert doc.uri == p.as_uri()
         assert doc.title == p.stem
         assert doc.body != ""
         assert isinstance(doc.metadata, dict)
