1199942608cb2a7bd320949101356926a45253c3
dnlzrgz <24715931+dnlzrgz@users.noreply.github.com>
2025-04-28 10:55:49 +0200

    fix: uri not being parsed correctly

--- chercher_plugin_md/__init__.py
+++ chercher_plugin_md/__init__.py
@@ -1,24 +1,30 @@
-import hashlib
 from pathlib import Path
 from typing import Generator
+from urllib.parse import urlparse
+import hashlib
 import frontmatter
 from chercher import Document, hookimpl
 
 
 @hookimpl
 def ingest(uri: str) -> Generator[Document, None, None]:
-    path = Path(uri).resolve()
+    parsed_uri = urlparse(uri)
+    if parsed_uri.scheme != "file":
+        return
+
+    path = Path(parsed_uri.path).resolve()
     if not path.exists() or not path.is_file() or path.suffix != ".md":
         return
 
     with path.open("rb") as f:
-        digest = hashlib.file_digest(f, "sha256")
-        post = frontmatter.loads(f.read().decode("utf-8"))
+        content = f.read()
+        hash = hashlib.sha256(content)
+        post = frontmatter.loads(content.decode("utf-8"))
 
     yield Document(
         uri=path.as_uri(),
         title=path.stem,
         body=post.content,
-        hash=digest.hexdigest(),
+        hash=hash.hexdigest(),
         metadata=post.metadata,
     )
--- tests/test_chercher_plugin_md.py
+++ tests/test_chercher_plugin_md.py
@@ -16,12 +16,13 @@ def test_valid_file(tmp_path):
     p.write_text(CONTENT)
 
     uri = p.as_uri()
-    documents = ingest(uri=uri)
+    documents = list(ingest(uri=uri))
+    assert len(documents) == 1
     for doc in documents:
         assert isinstance(doc, Document)
         assert doc.uri == uri
         assert doc.title == p.stem
-        assert doc.body == CONTENT
+        assert doc.body != ""
         assert isinstance(doc.metadata, dict)
         assert doc.metadata["title"] == "TDD"
         assert doc.hash is not None
@@ -32,17 +33,17 @@ def test_invalid_file(tmp_path):
     p.write_text(CONTENT)
 
     uri = p.as_uri()
-    documents = ingest(uri=uri)
-    assert list(documents) == []
+    documents = list(ingest(uri=uri))
+    assert documents == []
 
 
 def test_missing_file(tmp_path):
     p = tmp_path / "missingno.md"
-    documents = ingest(uri=p.as_uri())
-    assert list(documents) == []
+    documents = list(ingest(uri=p.as_uri()))
+    assert documents == []
 
 
 def test_invalid_uri():
     uri = "https://blog/post.md"
-    documents = ingest(uri=uri)
-    assert list(documents) == []
+    documents = list(ingest(uri=uri))
+    assert documents == []
